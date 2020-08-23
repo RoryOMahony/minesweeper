5b9ab134d16a75b34baae33142a6007b726ac659
Rory O'Mahony <rory1510@hotmail.co.uk>
2020-08-23 13:49:17 +0100

    Updated how the cell component looks

--- src/components/gamegrid/Cell.js
+++ src/components/gamegrid/Cell.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./Cell.css";
 
 const Cell = () => {
-  return <div className="cell"></div>;
+  return <div className="cell-container">
+    <div className="cell"></div>
+  </div>;
 };
 
 export default Cell;
