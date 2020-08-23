1f0ae0f91c877fc06091d2c212d83e76c793ecb4
Rory O'Mahony <rory1510@hotmail.co.uk>
2020-08-23 13:22:01 +0100

    Updated the minesweeper component to display the game cells

--- src/components/Minesweeper.js
+++ src/components/Minesweeper.js
@@ -1,10 +1,11 @@
 import React from "react";
+import GameGrid from "./gamegrid/GameGrid";
 import "./Minesweeper.css";
 
 export const Minesweeper = () => {
   return (
     <div id="game">
-
+      <GameGrid rows={10} columns={10}></GameGrid>
     </div>
   );
 };
