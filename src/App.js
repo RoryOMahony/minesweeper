b43334df033362e1a71242200731e29033e8b51a
Rory O'Mahony <rory1510@hotmail.co.uk>
2020-08-23 13:11:13 +0100

    Updated the App component to render the Minesweeper game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Title from "./components/Title";
+import { Minesweeper } from "./components/Minesweeper";
+import "./components/Flex.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="title-container">
+        <Title text="Minesweeper"></Title>
+      </div>
+      <div id="game-container" className="flex-row flex-main-axis-center">
+        <Minesweeper></Minesweeper>
+      </div>
     </div>
   );
 }
