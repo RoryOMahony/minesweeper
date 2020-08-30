5e25f11ec26b584ee17cf0c8b1ea91105d4795a0
Rory O'Mahony <rory1510@hotmail.co.uk>
2020-08-30 10:08:05 +0100

    Created a class that contains the difficulty levels

---
+++ src/components/difficulty/Difficulties.js
@@ -0,0 +1,20 @@
+export const DIFFICULTIES = {
+  BEGINNER: {
+    title: "Beginner",
+    rows: 10,
+    columns: 10,
+    mines: 10
+  },
+  INTERMEDIATE: {
+    title: "Intermediate",
+    rows: 16,
+    columns: 16,
+    mines: 40
+  },
+  EXPERT: {
+    title: "Expert",
+    rows: 16,
+    columns: 30,
+    mines: 99
+  }
+};
