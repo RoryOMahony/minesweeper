f864de9a4aa9400dabd9d5df672fbfec7ed51c3f
Rory O'Mahony <rory1510@hotmail.co.uk>
2020-08-30 10:17:40 +0100

    The rest button is now displayed to the user

--- src/components/gameinfo/GameInfo.js
+++ src/components/gameinfo/GameInfo.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import "../Flex.css"
-import NumberDisplay from './NumberDisplay'
+import React from "react";
+import "../Flex.css";
+import NumberDisplay from "./NumberDisplay";
+import GameControl from "./GameControl";
 
-const GameInfo = ({game}) => {
+const GameInfo = ({ game, gameDispatch }) => {
   return (
     <div className="flex-row flex-main-axis-space-between">
       <NumberDisplay number={game.flagsAvailable}></NumberDisplay>
+      <GameControl
+        gameState={game.gameState}
+        gameDispatch={gameDispatch}
+      ></GameControl>
       <NumberDisplay number={game.score}></NumberDisplay>
     </div>
-  )
-}
+  );
+};
 
 export default GameInfo;
