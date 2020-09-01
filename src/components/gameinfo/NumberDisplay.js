6b258897c1d5c913e3d15e2e69b7224d14dc5529
Rory O'Mahony <rory1510@hotmail.co.uk>
2020-09-01 13:27:39 +0100

    Updated how the score and the remaining flags are displayed

--- src/components/gameinfo/NumberDisplay.js
+++ src/components/gameinfo/NumberDisplay.js
@@ -3,7 +3,18 @@ import "./NumberDisplay.css";
 import "../Flex.css";
 
 const NumberDisplay = ({ number, displayStyle }) => {
-  return <div className={`number-display ${displayStyle}`}>{number}</div>;
+  function padNumber(number) {
+    if (number < 0) {
+      const positiveNum = number * -1;
+      const paddedNum = positiveNum.toString().padStart(2, 0);
+      return "-" + paddedNum;
+    }
+    return number.toString().padStart(3, 0);
+  }
+
+  const displayValue = padNumber(number);
+
+  return <div className={`number-display ${displayStyle}`}>{displayValue}</div>;
 };
 
 export default NumberDisplay;
