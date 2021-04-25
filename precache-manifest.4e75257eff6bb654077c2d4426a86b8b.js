d38f77a2516506e046d10801c835216cfe0d16cb
Rory O'Mahony <rory1510@hotmail.co.uk>
2021-04-25 15:41:15 +0100

    Updates

---
+++ precache-manifest.4e75257eff6bb654077c2d4426a86b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a08be641798e6aa558a8b73d38b2cd",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "60df470f088d2e8f5c15",
+    "url": "/minesweeper/static/css/main.72beba78.chunk.css"
+  },
+  {
+    "revision": "325173054a94b315a650",
+    "url": "/minesweeper/static/js/2.3f4bd9e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.3f4bd9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60df470f088d2e8f5c15",
+    "url": "/minesweeper/static/js/main.b79e3684.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
